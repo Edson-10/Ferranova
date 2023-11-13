8d33e5db79ddca70ef2d27b9d805c766b8c1bc03
Jair <i1921197@continental.edu.pe>
2023-11-13 13:28:33 -0500

    dsds

--- src/app/Services/usuario.service.ts
+++ src/app/Services/usuario.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../Interfaces/response-api';
+import { Login } from '../Interfaces/login';
+import { Usuario } from '../Interfaces/usuario';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  private urlApi: string = environment.endpoint + "Usuario/"
+  
+  constructor(private http: HttpClient) { }
+
+  iniciarSesion(request: Login): Observable<ResponseApi>{
+
+    return this.http.post<ResponseApi>('${this.urlApi}IniciarSesion' , request)
+
+  }
+
+
 }
