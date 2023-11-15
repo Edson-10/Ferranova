13dec97ca31b6e18b60852cd3c97a3ca85b25a30
Jair <i1921197@continental.edu.pe>
2023-11-14 22:02:04 -0500

    csdcsd

--- src/app/Components/layout/Modales/modal-usuario/modal-usuario.component.ts
+++ src/app/Components/layout/Modales/modal-usuario/modal-usuario.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Rol } from 'src/app/Interfaces/rol';
+import { Usuario } from 'src/app/Interfaces/usuario';
+
+import { RolService } from 'src/app/Services/rol.service';
+import { UsuarioService } from 'src/app/Services/usuario.service';
+import { UtilidadService } from 'src/app/Reutilizable/utilidad.service';
 
 @Component({
   selector: 'app-modal-usuario',
   templateUrl: './modal-usuario.component.html',
   styleUrls: ['./modal-usuario.component.css']
 })
-export class ModalUsuarioComponent {
+export class ModalUsuarioComponent implements OnInit {
+
+  fomularioUsuario: FormGroup;
+  ocultarPassword: boolean = true;
+  tituloAccion: string = "Agregar";
+  botonAccion: String = "Guardar";
+  listaRoles: Rol[]=[];
+
+  constructor(
+    private modalActual:MatDialogRef<ModalUsuarioComponent>,
+    @Inject(MAT_DIALOG_DATA) public dataUsuario:Usuario,
+    private fb: FormBuilder,
+    private _rolService: RolService,
+    private _usuarioServicio: UsuarioService,
+    private _utilidadServicio: UtilidadService
+  ){
+
+    this.fomularioUsuario = this.fb.group({
+      nombreCompleto : ['',Validators.required],
+      correo : ['',Validators.required],
+      idRol : ['',Validators.required],
+      clave : ['',Validators.required],
+      esActivo : ['1', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
